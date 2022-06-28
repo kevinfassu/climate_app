22ca221b75cbb024e0c40e8dd26e2ce9cb1d8365
livelovebll <70596067+livelovebll@users.noreply.github.com>
2022-06-28 10:35:34 -0700

    re structured html

--- script.js
+++ script.js
@@ -74,8 +74,86 @@ function queryData(cityName) {
             document.getElementById('tempNow').innerHTML = 'Temperature: ' + Math.round(weatherData.current.temp) + '°F';
             document.getElementById('UVI').innerHTML = 'UV Index: ' + weatherData.current.uvi;
 
-
+            getSearchHistory();
         })
     });
 };
+//next 5 days
+function fiveDayforecast(forecast) {
+    // clear 5-day forecast data, if there is any
+    $('#fiveDayContainer').empty();
+    // remove old data add new
+    for (let i = 0; i < 5; i++) {
+        displayDailyforecast(forecast.daily[i]);
+    }
+};
 
+function displayDailyforecast(fiveDayData) {
+    // retrieve the data we need from what was returned from the fetch requests
+    const date = Intl.DateTimeFormat('en-US').format(new Date(fiveDayData.dt * 1000));
+    const typePicture = fiveDayData.weather[0].icon;
+    const TEMP = fiveDayData.temp.day;
+    const HUMIDITY = fiveDayData.humidity;
+    const WIND = fiveDayData.wind_speed;
+
+    // dynamically create cards for each of the next 5 days weather
+    const dailyCard = `
+        <div class="column col s12 m6">
+            <div class="card">
+                <ul class="list-group list-group-flush">
+                    <h4 class="list-group-item date">${date}</h4>
+                    <img class="list-group-item weather-icon" src="https://openweathermap.org/img/wn/${typePicture}@2x.png" alt="Picture of the weather type">
+                    <li class="list-group-item temp">Temperature: ${Math.round(TEMP)}°F </li>
+                    <li class="list-group-item wind">Wind Speed: ${WIND} </li>
+                    <li class="list-group-item humidity">Humidity: ${HUMIDITY}% </li>
+                </ul>
+            </div>
+        </div>
+    `;
+
+    // append the card to the parent element
+    $('#fiveDayContainer').append(dailyCard);
+};
+
+function saveSearchHistory(cityName) {
+    searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+    if (!searchHistory.includes(cityName)) {
+        searchHistory.push(cityName);
+    }
+
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+};
+
+// get search history from localStorage
+function getSearchHistory() {
+    let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+    // clear SH buttons of any content they have
+    $("#searchHistoryBtns").empty();
+    // if there is search history in localStorage
+    if (searchHistory.length > 0) {
+        // take each one
+        for(let i = 0; i < searchHistory.length; i++) {
+            // create a button element for it
+            let historyItem = $('<button>').attr('class', 'btn btn-secondary searchHistoryBtns').text(searchHistory[i]);
+            // and append it to the parent element
+            $("#searchHistoryBtns").append(historyItem);
+        }
+    }
+};
+
+// load search history from localStorage
+getSearchHistory();
+
+
+
+// add event listener to search form
+citySearch.addEventListener('submit', formSubmit);
+
+$(document).on('click', '.searchHistoryBtns', function(onClick) {
+    onClick.preventDefault();
+    this.value = '';
+    const displayClickedCity = this.textContent;
+    queryData(displayClickedCity);
+});
